63e815f66c62503fddfc5b2df4f6eeb8e2451e31
RELX <93602499+RELXOP@users.noreply.github.com>
2024-09-02 21:16:17 +0530

    Add files via upload

---
+++ Quiz Website/index.js
@@ -0,0 +1,165 @@
+const startBtn = document.querySelector('.start-btn');
+const Btn = document.querySelector('.popup-btn');
+const exitBtn = document.querySelector('.exit-btn');
+const popupInfo = document.querySelector('.popup-info');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgain = document.querySelector('.tryAgain-btn');
+const goHome = document.querySelector('.goHome-btn');
+
+
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove('active');
+    main.classList.remove ('active');
+}
+
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove ('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
+    questionCounter(1);
+    headerScore();
+}
+
+tryAgain.onclick = () => {
+    quizSection.classList.remove('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb)
+    headerScore();
+}
+
+goHome.onclick = () => {
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb)
+    headerScore();
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+   if (questionCount < questions.length - 1) {
+       questionCount++;
+       showQuestions(questionCount);
+
+       questionNumb++;
+       questionCounter(questionNumb)
+
+       nextBtn.classList.remove('active');
+    }
+    else {
+        showResultBox();
+    }
+}
+
+const optionList = document.querySelector('.option-list');
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+                     <div class="option"><span>${questions[index].options[1]}</span></div>
+                     <div class="option"><span>${questions[index].options[2]}</span></div>
+                     <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i=0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = ` ${index} of ${questions.length} Questions`;
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+    }
+    else {
+        answer.classList.add('incorrect');
+
+        for (let i=0; i < allOptions; i++) {
+            if (optionList.children[i].textContent == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+    }
+
+    for (let i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+}
+
+
+function headerScore (){
+    const headerScoreText  = document.querySelector('.header-score');
+    headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`
+
+    const cicularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+
+    let progressStartValue = 0;
+    let progressEndValue = (userScore / questions.length) * 100;
+
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+        progressValue.textContent = `${progressEndValue}`
+       cicularProgress.style.backgorund = `conic-gradient(#00ffff ${progressStartValue * 3.6}deg, rgba(255,255,255,0.1) 0deg)`;
+
+
+        if (progressStartValue == progressEndValue) {
+            clearInterval(progress);
+        }
+    }, speed);
+}
---
+++ Quiz Website/question.js
@@ -0,0 +1,69 @@
+let questions = [
+    {
+        numb:1,
+        question:"Which of the following can read and render HTML web pages?",
+        answer:"C. web browser",
+        options: [
+            "A. Server",
+            "B. head Tak",
+            "C. web browser",
+            "D. empty"
+        ]
+    },
+    {
+        numb:2,
+        question:"The latest HTML standard is ?",
+        answer:"B. HTML5.0",
+        options: [
+            "A. HTML4.0",
+            "B. HTML5.0",
+            "C. XML",
+            "D. SGML"
+        ]
+    },
+    {
+        numb:3,
+        question:"Simple network management protocol uses which of the following port number ?",
+        answer:"D. 161",
+        options: [
+            "A. 164",
+            "B. 163",
+            "C. 160",
+            "D. 161"
+        ]
+    },
+    {
+        numb:4,
+        question:"Which of the following can read and render HTML web pages?",
+        answer:"A. For server-side programming",
+        options: [
+            "A. For server-side programming",
+            "B. For client-side programming",
+            "C. both a and b",
+            "D. NONE"
+        ]
+    },
+    {
+        numb:5,
+        question:"Identify the incorrect HTML tag among the following",
+        answer:"C. < list >",
+        options: [
+            "A. < input >",
+            "B. < select >",
+            "C. < list >",
+            "D. < textarea >"
+        ]
+    },
+    {
+        numb:6,
+        question:"What are variables used in JavaScript programs",
+        answer:"B. Store numbers, dates, and other values.",
+        options: [
+            "A. Varying randomly",
+            "B. Store numbers, dates, and other values.",
+            "C. Used as header files",
+            "D. none of the above"
+        ]
+    }
+    
+];
